1533b7ac87adeadcd73f10844ebe4f7b458b7646
Jonas Forsberg <jonas.forsberg@suse.com>
2019-03-10 17:28:16 +0100

    black compatible

--- mydot.py
+++ mydot.py
@@ -10,47 +10,46 @@ import socket
 import shutil
 from pathlib import Path
 
-LOGGING_STRING='[%(levelname)s] %(message)s'
+LOGGING_STRING = "[%(levelname)s] %(message)s"
 
 logger = logging.getLogger(__name__)
 
 dotdir = None
+
 
 class MyDot(object):
     path = None
     repo = None
     uid = None
 
-
-
     def __init__(self, path):
         self.path = Path(path)
         self.uid = socket.gethostname()
 
 
-
 class ColorLogFormater(logging.Formatter):
     COLOR = {
-            logging.ERROR: colorama.Fore.RED,
-            logging.CRITICAL: colorama.Fore.RED,
-            logging.WARNING: colorama.Fore.YELLOW,
-            logging.DEBUG: colorama.Fore.BLUE
-            }
+        logging.ERROR: colorama.Fore.RED,
+        logging.CRITICAL: colorama.Fore.RED,
+        logging.WARNING: colorama.Fore.YELLOW,
+        logging.DEBUG: colorama.Fore.BLUE,
+    }
     STYLE = {
-            logging.ERROR: colorama.Style.BRIGHT,
-            logging.CRITICAL: colorama.Style.BRIGHT,
-            logging.WARNING: colorama.Style.BRIGHT,
-            logging.DEBUG:  colorama.Style.BRIGHT
-            }
+        logging.ERROR: colorama.Style.BRIGHT,
+        logging.CRITICAL: colorama.Style.BRIGHT,
+        logging.WARNING: colorama.Style.BRIGHT,
+        logging.DEBUG: colorama.Style.BRIGHT,
+    }
 
     def format(self, record, *args, **kwargs):
         theRecord = copy.copy(record)
         if theRecord.levelno in self.COLOR:
             theRecord.levelname = "{color}{style}{level}{reset}".format(
-                    color=self.COLOR[theRecord.levelno],
-                    style=self.STYLE[theRecord.levelno],
-                    level=record.levelname,
-                    reset=colorama.Style.RESET_ALL)
+                color=self.COLOR[theRecord.levelno],
+                style=self.STYLE[theRecord.levelno],
+                level=record.levelname,
+                reset=colorama.Style.RESET_ALL,
+            )
         return super(ColorLogFormater, self).format(theRecord, *args, **kwargs)
 
 
@@ -67,31 +66,46 @@ def format_logger(loglevel, nocolor=False):
 
 
 @click.group()
-@click.option('--path', type=click.Path(), default="{}/.dotfiles".format(Path.home()), help="set root of dotdir git folder [default = ~/.dotfiles]")
-@click.option('--loglevel', type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "NOTSET"]), default="WARNING", help="Set the logging level")
+@click.option(
+    "--path",
+    type=click.Path(),
+    default="{}/.dotfiles".format(Path.home()),
+    help="set root of dotdir git folder [default = ~/.dotfiles]",
+)
+@click.option(
+    "--loglevel",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "NOTSET"]),
+    default="WARNING",
+    help="Set the logging level",
+)
 @click.pass_context
 def main(ctx, path, loglevel):
     global dotdir
     format_logger(loglevel)
-    logger.debug('Starting..')
+    logger.debug("Starting..")
     dotdir = MyDot(os.path.abspath(path))
-    if ctx.invoked_subcommand != 'init':
+    if ctx.invoked_subcommand != "init":
         if dotdir.path.exists() and dotdir.path.is_dir():
-            logger.debug('Loading git repo')
+            logger.debug("Loading git repo")
             try:
                 dotdir.repo = git.Repo(dotdir.path.as_posix())
             except git.InvalidGitRepositoryError as e:
-                logger.critical('Invalit git repository: {}'.format(e))
+                logger.critical("Invalit git repository: {}".format(e))
                 sys.exit(1)
         else:
             if not dotdir.path.exists():
-                logger.critical('Local repo dont exists {}, you might want to run init'.format(dotdir.path.as_posix()))
+                logger.critical(
+                    "Local repo dont exists {}, you might want to run init".format(
+                        dotdir.path.as_posix()
+                    )
+                )
                 sys.exit(1)
-            logger.critical('{} is not a directory'.format(dotdir.path.as_posix()))
+            logger.critical("{} is not a directory".format(dotdir.path.as_posix()))
             sys.exit(1)
 
+
 @main.command()
-@click.option('--silent', is_flag=True, help="no output, exit code 0 if in sync")
+@click.option("--silent", is_flag=True, help="no output, exit code 0 if in sync")
 @click.pass_context
 def status(ctx, silent):
     """ Invoke git repo commands..."""
@@ -103,175 +117,206 @@ def status(ctx, silent):
             sys.exit(1)
         if dotdir.repo.index.diff("HEAD"):
             sys.exit(1)
-#        ahead = dotdir.repo.iter_commits('origin/master..master')
-#        if sum(1 for c in ahead):
-#            sys.exit(1)
-#        behind = dotdir.repo.iter_commits('master..origin/master')
-#        if sum(1 for c in behind):
-#            sys.exit(1)
         sys.exit(0)
-    print('Repo description: {}'.format(dotdir.repo.description))
-    print('Last commit for repo is {}.'.format(str(dotdir.repo.head.commit.hexsha)))
-    commits_behind = dotdir.repo.iter_commits('master..origin/master')
-    commits_ahead = dotdir.repo.iter_commits('origin/master..master')
+    print("Repo description: {}".format(dotdir.repo.description))
+    print("Last commit for repo is {}.".format(str(dotdir.repo.head.commit.hexsha)))
+    commits_behind = dotdir.repo.iter_commits("master..origin/master")
+    commits_ahead = dotdir.repo.iter_commits("origin/master..master")
     for remote in dotdir.repo.remotes:
         print('    Remote named "{}" with URL "{}"'.format(remote, remote.url))
 
     if sum(1 for c in commits_ahead):
-        commits_ahead = dotdir.repo.iter_commits('origin/master..master')
-        print('\n{}{}Commits ahead{}:'.format(colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL))
+        commits_ahead = dotdir.repo.iter_commits("origin/master..master")
+        print(
+            "\n{}{}Commits ahead{}:".format(
+                colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL
+            )
+        )
         for commit in commits_ahead:
-            print('  * {} : {}'.format(str(commit.authored_datetime), commit.summary))
+            print("  * {} : {}".format(str(commit.authored_datetime), commit.summary))
     if dotdir.repo.is_dirty():
-        print('\n{}{}Changed files{}:'.format(colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL))
+        print(
+            "\n{}{}Changed files{}:".format(
+                colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL
+            )
+        )
         for item in dotdir.repo.index.diff(None):
-            print('  * {}'.format(item.a_path))
+            print("  * {}".format(item.a_path))
 
     if dotdir.repo.untracked_files:
-        print('\n{}{}Untracked files{}:'.format(colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL))
+        print(
+            "\n{}{}Untracked files{}:".format(
+                colorama.Style.BRIGHT, colorama.Fore.YELLOW, colorama.Style.RESET_ALL
+            )
+        )
         for f in dotdir.repo.untracked_files:
-            print('  * {}'.format(f))
+            print("  * {}".format(f))
+
 
 @main.command()
-@click.argument('fname')
+@click.argument("fname")
 @click.pass_context
 def remove(ctx, fname):
     """Remove file from dotdir, it will remove the link and move the file from the repository"""
     global dotdir
     fname = os.path.abspath(fname)
     if not fname.startswith(dotdir.path.as_posix()):
-        logger.error('{} is not in your repository [{}]'.format(fname, dotdir.path.as_posix()))
+        logger.error(
+            "{} is not in your repository [{}]".format(fname, dotdir.path.as_posix())
+        )
         sys.exit(1)
 
-    destfile = '{home}/{path}'.format(
+    destfile = "{home}/{path}".format(
         home=Path.home().as_posix(),
-        path=fname.replace(dotdir.path.as_posix()+'/'+dotdir.uid+'/', '')
-        )
-
+        path=fname.replace(dotdir.path.as_posix() + "/" + dotdir.uid + "/", ""),
+    )
 
     if Path(destfile).is_symlink():
         if not os.readlink(destfile) == fname:
-            logger.error("The target symlink is pointing to wrong file [{}]".format(os.readlink(destfile)))
+            logger.error(
+                "The target symlink is pointing to wrong file [{}]".format(
+                    os.readlink(destfile)
+                )
+            )
             sys.exit(1)
     else:
         logger.error("The target {} is not a symlink".format(destfile))
         sys.exit(1)
 
-    logger.debug('Unlinking {}'.format(destfile))
+    logger.debug("Unlinking {}".format(destfile))
     os.unlink(destfile)
-    logger.debug('Copy {} > {}'.format(fname, destfile))
+    logger.debug("Copy {} > {}".format(fname, destfile))
     shutil.copy(fname, destfile)
-    logger.debug('Removing {} from repo'.format(fname))
-    gitfile=fname.replace(dotdir.path.as_posix()+'/', '')
+    logger.debug("Removing {} from repo".format(fname))
+    gitfile = fname.replace(dotdir.path.as_posix() + "/", "")
     if gitfile in dotdir.repo.untracked_files:
-        logger.debug('{} is not commited yet'.format(fname))
+        logger.debug("{} is not commited yet".format(fname))
         if Path(fname).is_symlink():
-            logger.debug('{} is a symlink, unlinking'.format(fname))
+            logger.debug("{} is a symlink, unlinking".format(fname))
             os.unlink(fname)
         else:
-            logger.debug('removing {}'.format(fname))
+            logger.debug("removing {}".format(fname))
             os.remove(fname)
     else:
-        logger.debug('{} is commited, removing file'.format(gitfile))
-        dotdir.repo.index.remove([gitfile],working_tree = True)
-        dotdir.repo.index.commit('mydot: removed {}'.format(gitfile))
+        logger.debug("{} is commited, removing file".format(gitfile))
+        dotdir.repo.index.remove([gitfile], working_tree=True)
+        dotdir.repo.index.commit("mydot: removed {}".format(gitfile))
 
 
 @main.command()
-@click.option('--message', default=None, help='Commit message')
+@click.option("--message", default=None, help="Commit message")
 @click.pass_context
 def commit(ctx, message):
     """Commit changed files"""
     global dotdir
     if not dotdir.repo.is_dirty():
-        logger.info('Nothing to commit')
+        logger.info("Nothing to commit")
         return
     for item in dotdir.repo.index.diff(None):
-        logger.info('Adding: {}'.format(item.a_path))
+        logger.info("Adding: {}".format(item.a_path))
     if message == None:
-        message=input('Commit message: ')
+        message = input("Commit message: ")
     for item in dotdir.repo.index.diff(None):
         dotdir.repo.index.add([item.a_path])
     dotdir.repo.index.commit(message)
+
 
 @main.command()
 @click.pass_context
 def push(ctx):
     """Push remotes"""
     global dotdir
-    logger.debug('Pushing to remotes')
-    dotdir.repo.remotes.origin.push('master')
+    logger.debug("Pushing to remotes")
+    dotdir.repo.remotes.origin.push("master")
+
 
 @main.command()
 @click.pass_context
 def pull(ctx):
     """Pull remotes"""
     global dotdir
-    logger.debug('Pulling from remotes')
-    dotdir.repo.remotes.origin.pull('master')
+    logger.debug("Pulling from remotes")
+    dotdir.repo.remotes.origin.pull("master")
+
 
 @main.command()
-@click.argument('url')
+@click.argument("url")
 @click.pass_context
 def init(ctx, url):
     """Init a local repository with URL as remotei origin"""
     global dotdir
     if dotdir.path.exists():
-        logger.critical('Can not init local repo when {} already exists'.format(dotdir.path.as_posix()))
+        logger.critical(
+            "Can not init local repo when {} already exists".format(
+                dotdir.path.as_posix()
+            )
+        )
         sys.exit(1)
-    logger.info('Creating local repo: {}'.format(dotdir.path.as_posix()))
+    logger.info("Creating local repo: {}".format(dotdir.path.as_posix()))
     repo = git.Repo.init(dotdir.path.as_posix())
-    logger.info('Adding remote origin: {}'.format(url))
-    origin = repo.create_remote('origin', url)
-    logger.info('Fetching origin')
+    logger.info("Adding remote origin: {}".format(url))
+    origin = repo.create_remote("origin", url)
+    logger.info("Fetching origin")
     origin.fetch()
-    logger.info('Remote pull')
+    logger.info("Remote pull")
     origin.pull(origin.refs[0].remote_head)
 
 
 @main.command()
-@click.argument('source')
+@click.argument("source")
 @click.pass_context
 def add(ctx, source):
     """Add a file to managed dotfile"""
     global dotdir
     source = os.path.abspath(source)
     """Check if Source is located in home directory and not in dotdir"""
-    logger.debug('Checking if {} is valid to add'.format(source))
+    logger.debug("Checking if {} is valid to add".format(source))
     if not source.startswith(str(Path.home())):
-        logger.critical('You can only add files from your home directory')
+        logger.critical("You can only add files from your home directory")
         sys.exit(1)
     if source.startswith(str(dotdir.path.as_posix())):
-        logger.critical('You can not add files from your local mydot git repo')
+        logger.critical("You can not add files from your local mydot git repo")
         sys.exit(1)
 
-    destfile = '{repo}/{uid}{path}'.format(
+    destfile = "{repo}/{uid}{path}".format(
         repo=dotdir.path.as_posix(),
         uid=dotdir.uid,
-        path=source.replace(Path.home().as_posix(), '')
-        )
+        path=source.replace(Path.home().as_posix(), ""),
+    )
     if os.path.isfile(destfile):
-        logger.error("There's already a file in ~{}".format(destfile.replace(Path.home().as_posix(), "")))
+        logger.error(
+            "There's already a file in ~{}".format(
+                destfile.replace(Path.home().as_posix(), "")
+            )
+        )
         sys.exit(1)
     if os.path.isdir(destfile):
-        logger.error("~{} is an existing directory".format(destfile.replace(Path.home().as_posix(), "")))
+        logger.error(
+            "~{} is an existing directory".format(
+                destfile.replace(Path.home().as_posix(), "")
+            )
+        )
         sys.exit(1)
-    
-    if os.path.exists(os.path.dirname(destfile)) and os.path.isfile(os.path.dirname(destfile)):
-        logger.error('Destination directory [~{}] is a file'.format(os.path.dirname(destfile)))
+
+    if os.path.exists(os.path.dirname(destfile)) and os.path.isfile(
+        os.path.dirname(destfile)
+    ):
+        logger.error(
+            "Destination directory [~{}] is a file".format(os.path.dirname(destfile))
+        )
         sys.exit(1)
     elif not os.path.exists(os.path.dirname(destfile)):
-        logger.debug('Creating directory ~{}'.format(os.path.dirname(destfile)))
+        logger.debug("Creating directory ~{}".format(os.path.dirname(destfile)))
         os.makedirs(os.path.dirname(destfile))
-    
-    logger.debug('Moving file to repo')
+
+    logger.debug("Moving file to repo")
     os.rename(source, destfile)
-    logger.debug('Creating symlink')
+    logger.debug("Creating symlink")
     os.symlink(destfile, source)
-    logger.debug('Commiting file in repo')
-    gitfile=destfile.replace(dotdir.path.as_posix()+'/', '')
+    logger.debug("Commiting file in repo")
+    gitfile = destfile.replace(dotdir.path.as_posix() + "/", "")
     dotdir.repo.index.add([gitfile])
-    dotdir.repo.index.commit('mydot: adding {}'.format(gitfile))
+    dotdir.repo.index.commit("mydot: adding {}".format(gitfile))
 
 
 if __name__ == "__main__":
